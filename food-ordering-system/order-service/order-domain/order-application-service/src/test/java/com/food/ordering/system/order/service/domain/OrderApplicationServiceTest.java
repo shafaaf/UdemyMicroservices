b855349a02547e9565605fc5d34ef8414213c4e4
shafaaf <shafaaf.hossain@hotmail.com>
2023-03-18 13:08:45 -0400

    kds

---
+++ food-ordering-system/order-service/order-domain/order-application-service/src/test/java/com/food/ordering/system/order/service/domain/OrderApplicationServiceTest.java
@@ -0,0 +1,158 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.domain.valueobject.*;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderRequest;
+import com.food.ordering.system.order.service.domain.dto.create.OrderAddressDto;
+import com.food.ordering.system.order.service.domain.dto.create.OrderItemDto;
+import com.food.ordering.system.order.service.domain.entity.Customer;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.entity.Product;
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.inputs.OrderApplicationService;
+import com.food.ordering.system.order.service.domain.ports.outputs.repository.CustomerRepository;
+import com.food.ordering.system.order.service.domain.ports.outputs.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.ports.outputs.repository.RestaurantRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(classes = OrderTestConfiguration.class)
+public class OrderApplicationServiceTest {
+
+    @Autowired
+    private OrderApplicationService orderApplicationService;
+    @Autowired
+    private OrderDataMapper orderDataMapper;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    private CreateOrderRequest createOrderRequest;
+    private CreateOrderRequest createOrderRequestWrongTotalPrice;
+    private CreateOrderRequest createOrderRequestWrongProductPrice;
+    private final UUID CUSTOMER_ID = UUID.fromString("d215b5f8-0249-4dc5-89a3-51fd148cfb41");
+    private final UUID RESTAURANT_ID = UUID.fromString("d215b5f8-0249-4dc5-89a3-51fd148cfb45");
+    private final UUID PRODUCT_ID = UUID.fromString("d215b5f8-0249-4dc5-89a3-51fd148cfb48");
+    private final UUID ORDER_ID = UUID.fromString("15a497c1-0f4b-4eff-b9f4-c402c8c07afb");
+    private final BigDecimal PRICE = new BigDecimal("200.00");
+
+    @BeforeAll
+    public void init() {
+        createOrderRequest = CreateOrderRequest.builder()
+                .customerId(CUSTOMER_ID)
+                .restaurantId(RESTAURANT_ID)
+                .address(OrderAddressDto.builder()
+                        .street("street_1")
+                        .postalCode("1000AB")
+                        .city("Paris")
+                        .build())
+                .price(PRICE)
+                .items(List.of(
+                        OrderItemDto.builder()
+                                .productId(PRODUCT_ID)
+                                .quantity(1)
+                                .price(new BigDecimal("50.00"))
+                                .subTotal(new BigDecimal("50.00"))
+                                .build(),
+                        OrderItemDto.builder()
+                                .productId(PRODUCT_ID)
+                                .quantity(3)
+                                .price(new BigDecimal("50.00"))
+                                .subTotal(new BigDecimal("150.00"))
+                                .build()))
+                .build();
+
+        createOrderRequestWrongTotalPrice = CreateOrderRequest.builder()
+                .customerId(CUSTOMER_ID)
+                .restaurantId(RESTAURANT_ID)
+                .address(OrderAddressDto.builder()
+                        .street("street_1")
+                        .postalCode("1000AB")
+                        .city("Paris")
+                        .build())
+                .price(new BigDecimal("250.00"))
+                .items(List.of(
+                        OrderItemDto.builder()
+                                .productId(PRODUCT_ID)
+                                .quantity(1)
+                                .price(new BigDecimal("50.00"))
+                                .subTotal(new BigDecimal("50.00"))
+                                .build(),
+                        OrderItemDto.builder()
+                                .productId(PRODUCT_ID)
+                                .quantity(3)
+                                .price(new BigDecimal("50.00"))
+                                .subTotal(new BigDecimal("150.00"))
+                                .build()))
+                .build();
+
+        createOrderRequestWrongProductPrice = CreateOrderRequest.builder()
+                .customerId(CUSTOMER_ID)
+                .restaurantId(RESTAURANT_ID)
+                .address(OrderAddressDto.builder()
+                        .street("street_1")
+                        .postalCode("1000AB")
+                        .city("Paris")
+                        .build())
+                .price(new BigDecimal("210.00"))
+                .items(List.of(
+                        OrderItemDto.builder()
+                                .productId(PRODUCT_ID)
+                                .quantity(1)
+                                .price(new BigDecimal("60.00")) // wrong price here
+                                .subTotal(new BigDecimal("60.00"))
+                                .build(),
+                        OrderItemDto.builder()
+                                .productId(PRODUCT_ID)
+                                .quantity(3)
+                                .price(new BigDecimal("50.00"))
+                                .subTotal(new BigDecimal("150.00"))
+                                .build()))
+                .build();
+
+
+        Customer customer = Customer.builder()
+                .id(new CustomerId(CUSTOMER_ID))
+                .build();
+        Restaurant restaurant = Restaurant.builder()
+                .id(new RestaurantId(createOrderRequest.getRestaurantId()))
+                .products(
+                        List.of(
+                                Product.builder()
+                                        .id(new ProductId(PRODUCT_ID))
+                                        .name("product-1")
+                                        .price(new Money(new BigDecimal("50.00")))
+                                        .build(),
+                                Product.builder()
+                                        .id(new ProductId(PRODUCT_ID))
+                                        .name("product-2")
+                                        .price(new Money(new BigDecimal("50.00")))
+                                        .build()
+                        )
+                )
+                .active(true)
+                .build();
+
+        Order order = orderDataMapper.createOrderRequestToOrderEntity(createOrderRequest);
+        order.setId(new OrderId(ORDER_ID));
+
+        when(customerRepository.findCustomer(CUSTOMER_ID)).thenReturn(Optional.of(customer));
+        when(restaurantRepository.findRestaurantInformation(orderDataMapper.createOrderRequestToRestaurantEntity(createOrderRequest)))
+                .thenReturn(Optional.of(restaurant));
+        when(orderRepository.save(any(Order.class))).thenReturn(order);
+    }
+}
